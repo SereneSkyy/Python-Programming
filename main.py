36f9439a99ada8fde2edc2baea1ce208a37b5896
Saurav Khanal <sauravkhanal44@gmail.com>
2024-11-06 10:11:13 +0545

    W3_LastCommit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math_util
+import mathematics.trigonometry
+
+result = math_util.add(1,7)
+print(result)
+
+import mathematics
+from mathematics import trigonometry
+cosine_of_45 = mathematics.trigonometry.cosine(45)
+trigonometry.cosine(45)
+print(cosine_of_45)
