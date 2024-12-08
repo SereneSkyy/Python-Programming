3434af418858f545b4197368e2d75bc9937a3b76
Saurav Khanal <sauravkhanal44@gmail.com>
2024-12-08 18:01:07 +0545

    Ml contd..

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import math
+import logging
 import math_util
-import mathematics.trigonometry
+from mathematics.trigonometry import cosine, sine
 
 result = math_util.add(1,7)
 print(result)
@@ -9,3 +11,14 @@ from mathematics import trigonometry
 cosine_of_45 = mathematics.trigonometry.cosine(45)
 trigonometry.cosine(45)
 print(cosine_of_45)
+
+logging.debug("debug message")
+logging.info("info message")
+logging.warning("warning message")
+logging.error("error message")
+logging.critical("critical message")
+
+
+sin_45 = sine(math.pi/4)
+print(sin_45)
+
---
+++ mathematics/test_trigonometry.py
@@ -0,0 +1,19 @@
+import mathematics.trigonometry as trig
+from math import isclose
+import pytest
+
+# object oriented way of testing
+class TestTrigonometry:
+
+    def test_factorial(self):
+        assert trig.factorial(5) == 120
+
+    @pytest.mark.skip(reason="THis feature is under development")
+    def test_sec(self):
+        assert False
+
+#procedural way
+ 
+def test_cosine():
+    """Test the cosine function"""
+    assert isclose(trig.cosine(0),1, abs_tol=1e-6) 
